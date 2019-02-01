1eab3afbd48872969d35af74a200bfcb89d1106f
BullseyeHit <cedricgullentops@hotmail.com>
2019-02-01 23:26:29 +0100

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import quandl
 import datetime
-import matplotlib.pyplot as plt  # Import matplotlib
 
 # We will look at stock prices over the past year, starting at January 1, 2016
 start = datetime.datetime(2016, 1, 1)
@@ -12,6 +11,4 @@ end = datetime.date.today()
 s = "AAPL"
 apple = quandl.get("WIKI/" + s, start_date=start, end_date=end)
 
-type(apple)
-
-apple.head()
+print apple
